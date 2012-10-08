912e65db630f8e9a30d89afb8015bc21ac23ba64
Ruth Williams <rw269[at]yahoo.com>
2012-10-08 19:31:59 +0100

    First of today's files

---
+++ Ex1x18xPi.groovy
@@ -0,0 +1,21 @@
+print "When calculating Pi, how many terms should be added together? "
+String str = System.console().readLine()
+int termnum = Integer.parseInt(str)
+int x=1, y=1
+double sum=0
+ 
+while (y<=termnum)
+{
+	if (y % 2 != 0)
+ 	{
+  		sum=sum+(4/x)
+ 	}
+ 	else if (y % 2 = 0)
+ 	{
+  		sum=sum-(4/x)
+ 	}
+	x=x+2
+	y=y+1
+}
+ 
+println "Pi to " + termnum + " terms is " + sum
