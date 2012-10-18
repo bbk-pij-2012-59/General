b840c9bfaac2484d4e88eaf14885ef44a560cbd4
Ruth Williams <rw269[at]yahoo.com>
2012-10-18 19:46:26 +0100

    Day three, exercise six; finally working

--- Ex3x6.groovy
+++ Ex3x6.groovy
@@ -1,5 +1,4 @@
 //Ex3x6
-//Not working - in an infinite loop
 boolean match = true
 int count = 0, lenshortstr = 0, lenlongstr = 0
 String shortstr = "", longstr = ""
@@ -9,25 +8,22 @@ lenshortstr = shortstr.length()
 print "Please enter your longer text: "
 longstr = System.console().readLine()
 lenlongstr = longstr.length()
-
 for (int i = 0; i<=(lenlongstr-lenshortstr); i++)
 {
-	while (match)
-	{
-		for (int j = 0; j<=(lenshortstr-1); j++)
-		{
-		
-			if (shortstr.charAt(j) != longstr.charAt(i+j))
-			{
-				match = false
-			}
-		}
-		if (match)
-		{
-			count++
-		}
-	}
 	match = true
+	for (int j = 0; j<=(lenshortstr-1); j++)
+	{
+		if (shortstr.charAt(j) != longstr.charAt(i+j))
+		{
+			match = false
+		}
+//	println "i="+i+" j="+j+" match="+match+" count="+count
+	}
+	if (match)
+	{
+		count++
+	}
 }
 
-println "The short string " + shortstr + " occurs " + count + " times in your text."
+println "The short string (" + shortstr + ") occurs " + count + " times in your text."
+
