698375ceb726cdd217da15c01f7227222b64cc2e
Ruth Williams <rw269[at]yahoo.com>
2012-10-25 19:27:01 +0100

    Day three, exercise 16; some progress, but not perfect and hanging at end

--- Ex3x16.groovy
+++ Ex3x16.groovy
@@ -1,4 +1,5 @@
 //Ex3x16 - not yet tested
+//QUIT works! MAIL FROM almost works! RCPT TO almost works! DATA almost works! Hangs at end tho'
 boolean cmdquit = false, cmdmail = false, cmdrcpt = false, cmddata = false
 int str1len = 0, str2len = 0, emailtextlen = 0
 String str1 = "", str2 = "", str3 = "", str4 = "", emailtext = ""
@@ -9,22 +10,32 @@ while (!cmdquit)
 		print ">>> "
 		str1 = System.console().readLine()
 		str1len = str1.length()
-		if (str1.substring(0,4) == "QUIT")
-		{
-			cmdquit = true
-		}
-		else if (str1.substring(0,11) == "MAIL FROM: "
-		{
-			for (int i = 12; i <= (str1len-2); i++)
+		if (str1len == 4 && str1.substring(0,4) == "QUIT")
 			{
-				if (str1.substring(i,i+1) == "@"
-				{
-					cmdmail = true
-				}
+				cmdquit = true
 			}
-			if (!cmdmail)
+		else if (str1len >= 11)
+		{
+			if (str1.substring(0,11) == "MAIL FROM: ")
 			{
-				println "Invalid email address"
+				if (str1len >= 14)
+				{
+					for (int i = 12; i <= (str1len-2); i++)
+					{
+						if (str1.substring(i,i+1) == "@")
+						{
+							cmdmail = true
+						}
+					}
+				}
+				if (cmdmail)
+				{
+					println "OK"
+				}
+				else
+				{
+					println "Invalid email address"
+				}			
 			}
 		}
 		else
@@ -32,6 +43,7 @@ while (!cmdquit)
 			println "Invalid command."
 		}
 	}
+//OKish to here?	
 	if (!cmdquit)
 	{
 		while (!(cmdrcpt || cmdquit))
@@ -39,64 +51,88 @@ while (!cmdquit)
 			print ">>> "
 			str2 = System.console().readLine()
 			str2len = str2.length()
-			if (str2.substring(0,4) == "QUIT")
+			if (str2len == 4 && str2.substring(0,4) == "QUIT")
 			{
 				cmdquit = true
 			}
-			else if (str2.substring(0,9) == "RPCT TO: "
+			else if (str2len >= 9)
 			{
-				for (int j = 10; j <= (str2len-2); j++)
+				if (str2.substring(0,9) == "RPCT TO: ")
 				{
-					if (str2.substring(j,j+1) == "@"
+					if (str2len >= 12)
 					{
-						cmdrcpt = true
+						for (int j = 10; j <= (str2len-2); j++)
+						{
+							if (str2.substring(j,j+1) == "@")
+							{
+								cmdrcpt = true
+							}
+						}
 					}
-				}
-				if (!cmdrcpt)
-				{
-					println "Invalid email address"
+					if (cmdrcpt)
+					{
+						println "OK"
+					}
+					else
+					{
+						println "Invalid email address"
+					}
 				}
 			}
 			else
 			{
 				println "Invalid command."
 			}
-		}	
+		}
 	}
+//OKish to here?
+
+
 	if (!cmdquit)
 	{
 		while (!(cmddata || cmdquit))
 		{
 			print ">>> "
 			str3 = System.console().readLine()
-			if (str3.substring(0,4) == "QUIT")
+			str3len = str3.length()
+			if (str3len == 4)
 			{
-				cmdquit = true
-			}
-			else if (str3.substring(0,4) == "DATA"
-			{
-				while (!cmddata)
+				if (str3.substring(0,4) == "QUIT")
 				{
-					str4 = System.console().readLine()
-					if (str4 == ".")
-					{
-						cmddata=true
-					}
-					else
-					{
-						emailtext = emailtext + str4 + "/n"
-					}
+					cmdquit = true
 				}
-				println "Sending email..."
-				println "from: " + str1.substring(11,str1len-1)
-				println "to: " + str2.substring(9,str2len-1)
-				println "from: " + str1.substring(11,str1len-1)
-				emailtextlen = emailtext.length()
-				for (int k=0; k<= emailtextlen-1, k++)
-				{					
-					println emailtext.substring(k,k+1)
+				else if (str3.substring(0,4) == "DATA")
+				{
+					while (!cmddata)
+					{
+						str4 = System.console().readLine()
+						if (str4 == ".")
+						{
+							cmddata=true
+						}
+						else
+						{
+							emailtext = emailtext + str4 + "#"
+						}
+					}
+					println "Sending email..."
+					println "from: " + str1.substring(11,str1len)
+					println "to: " + str2.substring(9,str2len)
+					emailtextlen = emailtext.length()
+					for (int k=0; k<= emailtextlen-1; k++)
+					{
+						if (substring(k,k+1) == "#")
+						{
+							println ""
+						}
+						else
+						{
+							print emailtext.substring(k,k+1)
+						}
+					}
+					println ""
+					println "...done!"
 				}
-				println "...done!"
 			}
 			else
 			{
