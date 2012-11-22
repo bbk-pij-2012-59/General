e655cde98a9686d4ee75ae256ed5a2ee0219ea1a
Ruth Williams <rw269[at]yahoo.com>
2012-11-22 18:16:25 +0000

    Day eight, exercise three, parts one and two; working

---
+++ TreeIntSortedList.java
@@ -0,0 +1,109 @@
+//Ex8x3x2 Implementation as tree
+//22nd November 2012 - Created
+
+public class TreeIntSortedList implements IntSet
+{
+
+	private int value;
+	private TreeIntSortedList left;
+	private TreeIntSortedList right;
+
+	public TreeIntSortedList(int value)
+	{
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+
+	public void add(int newValue)
+	{
+		//adds a new int to the set; if it is there already, nothing happens
+		if (contains(newValue))
+		{
+			System.out.println("Set already contains this value");
+			return;
+		}
+		else if (newValue > this.value)
+		{
+			if (right == null)
+			{
+				right = new TreeIntSortedList(newValue);
+			}
+			else
+			{
+				right.add(newValue);
+			}
+		}
+		else
+		{
+			if (left == null)
+			{
+				left = new TreeIntSortedList(newValue);
+			}
+			else
+			{
+				left.add(newValue);
+			}
+		}
+	}
+
+	public boolean contains(int searchValue)
+	{
+		//returns true if the number is in the set, false otherwise
+
+		if (searchValue == this.value)
+		{
+			return true;
+		}
+		else if (searchValue > this.value)
+		{
+			if (right == null)
+			{
+				return false;
+			}
+			else
+			{
+				return right.contains(searchValue);
+			}
+		}
+		else
+		{
+			if (left == null)
+			{
+				return false;
+			}
+			else
+			{
+				return left.contains(searchValue);
+			}
+		}
+	}
+
+	public boolean containsVerbose(int searchValue)
+	{
+		//returns the same values as the former method, but for every element that is checked prints its value on screen
+		System.out.println("Value searched for: " + searchValue);
+		boolean answer = contains(searchValue);
+		return answer;
+	}
+
+	public String toString()
+	{
+		//returns a string with the values of the elements in the set separated by commas
+
+		String setAsString = "" + this.value;
+
+		if (left != null)
+		{
+			setAsString = setAsString + ", " + left.toString();
+		}
+
+		if (right != null)
+		{
+			setAsString = setAsString + ", " + right.toString();
+		}
+
+		return setAsString;
+	}
+
+}
