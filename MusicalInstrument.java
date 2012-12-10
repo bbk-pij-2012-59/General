8f55aaad437c6ad62ea5fc1a9bb6b32d315aa50c
Ruth Williams <rw269[at]yahoo.com>
2012-12-10 20:29:57 +0000

    Day eleven, exercise five

---
+++ MusicalInstrument.java
@@ -0,0 +1,11 @@
+/**
+*Ex11x5
+*10th December 2012 - Created
+*/
+class MusicalInstrument
+{
+	public void play()
+	{
+		System.out.println("Playing instrument");
+	}
+}
---
+++ WoodenObject.java
@@ -0,0 +1,11 @@
+/**
+*Ex11x5
+*10th December 2012 - Created
+*/
+class WoodenObject
+{
+	public void burn()
+	{
+		System.out.println("Burning wooden object");
+	}
+}
