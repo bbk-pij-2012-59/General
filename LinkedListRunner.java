9c5ed3c934ac7938f3dec9b37f61799c04d32f68
Ruth Williams <rw269[at]yahoo.com>
2013-01-16 18:36:40 +0000

    Day Thirteen, Exercise One; in progress

---
+++ LinkedListRunner.java
@@ -0,0 +1,24 @@
+/**
+*Ex13x1
+*14th January 2013 - Created
+*Using the Java Collection Library - linked lists
+*/
+import java.util.LinkedList;
+
+public class LinkedListRunner
+{
+
+	public static void main(String[] args)
+	{
+	LinkedList<String> ruthlist = new LinkedList<>();
+	ruthlist.add("aaa");
+	ruthlist.add("bbb");
+	ruthlist.add("ccc");
+	ruthlist.add("ddd");
+	ruthlist.add("eee");
+	ruthlist.add("fff");
+	ruthlist.add("ggg");
+	ruthlist.add("hhh");
+	ruthlist.add("iii");
+	}
+}
